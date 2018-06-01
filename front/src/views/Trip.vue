b572d87dc27d6a20a8008ebafc5000064879baec
Benoît Ripoche <ripoche.b@gmail.com>
2018-06-01 15:29:12 +0000

    add trip view

---
+++ front/src/views/Trip.vue
@@ -0,0 +1,14 @@
+<template>
+    <tripView></tripView>
+</template>
+
+<script>
+import tripView from '@/components/TripView'
+
+export default {
+  name: 'trip-view',
+  components: {
+    tripView
+  }
+}
+</script>
