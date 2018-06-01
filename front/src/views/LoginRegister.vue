2521d1585bd6cf3dacd629a08fde5d455af81429
Benoît Ripoche <ripoche.b@gmail.com>
2018-06-01 14:47:25 +0000

    add login-register page

---
+++ front/src/views/LoginRegister.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <loginForm v-if="!register" @register="toggleRegister"></loginForm>
+    <registerForm v-if="register" @register="toggleRegister"></registerForm>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import loginForm from '@/components/LoginForm'
+import registerForm from '@/components/RegisterForm'
+
+export default {
+  name: 'home',
+  data: function () {
+    return {
+      register: false
+    }
+  },
+  components: {
+    loginForm,
+    registerForm
+  },
+  methods: {
+    toggleRegister: function (register) {
+      this.register = register
+    }
+  }
+}
+</script>
